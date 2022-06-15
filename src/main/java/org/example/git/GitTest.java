cf56096e22ac6cdc83192719028e392a864c01b0
qi33 <1527238060@qq.com>
2022-06-15 23:41:43 +0800

    first commit

---
+++ src/main/java/org/example/git/GitTest.java
@@ -0,0 +1,10 @@
+package org.example.git;/*
+@author Qi33
+@data 2022-06-15--22:15
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("nihaoei!");
+    }
+}
