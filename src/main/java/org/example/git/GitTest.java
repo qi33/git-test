338c12c0cb4dc720848a2087517675c38b0709d6
qi33 <1527238060@qq.com>
2022-06-16 09:52:42 +0800

    second commit

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -6,5 +6,6 @@ package org.example.git;/*
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("nihaoei!");
+        System.out.println("nihaoqi!");
     }
 }
