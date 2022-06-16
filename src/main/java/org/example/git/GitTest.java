9815fd76351787292cf65d3f1e03515867e6ba4c
qi33 <1527238060@qq.com>
2022-06-16 09:54:23 +0800

    third commit

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("nihaoei!");
         System.out.println("nihaoqi!");
+        System.out.println("nihaoqi33!");
     }
 }
